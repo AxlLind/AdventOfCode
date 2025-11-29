032636c377a0865a6f222362dfa95972db2d5468
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2025-11-29 08:15:04 +0100

    fetch: print http body on errors

--- fetch.sh
+++ fetch.sh
@@ -7,13 +7,15 @@ if [[ $# != 2 ]]; then
   exit 1
 fi
 
-if [[ ! "$1" =~ ^20(1[5-9]|2[0-9])$ ]]; then
-  echo "Not a valid year: $1"
+year=$1
+if [[ ! "$year" =~ ^20(1[5-9]|2[0-9])$ ]]; then
+  echo "Not a valid year: $year"
   exit 1
 fi
 
-if [[ ! "$2" =~ ^(0[1-9]|1[0-9]|2[0-5])$ ]]; then
-  echo "Not a valid day: $2"
+day=$2
+if [[ ! "$day" =~ ^(0[1-9]|1[0-9]|2[0-5])$ ]]; then
+  echo "Not a valid day: $day"
   exit 1
 fi
 
@@ -25,10 +27,10 @@ fi
 TMPFILE=$(mktemp)
 trap 'rm -f "$TMPFILE"' EXIT
 
-curl "https://adventofcode.com/$1/day/${2#0}/input"            \
-  -s --fail --cookie "session=$AOC_SESSION"                    \
+curl "https://adventofcode.com/$year/day/${day#0}/input"       \
+  -s --fail-with-body --cookie "session=$AOC_SESSION"          \
   -A "Bash script at $(git remote -v | awk 'NR==1{print $2}')" \
   | tee "$TMPFILE"
 
-mkdir -p "$SCRIPT_DIR/$1/inputs"
-mv "$TMPFILE" "$SCRIPT_DIR/$1/inputs/$2.in"
+mkdir -p "$SCRIPT_DIR/$year/inputs"
+mv "$TMPFILE" "$SCRIPT_DIR/$year/inputs/$day.in"
