7f2f0f6012873b9ac752da9d5153ecd354646305
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2025-12-02 09:57:25 +0100

    rust: update template.rs

--- 2024/src/template.rs
+++ 2024/src/template.rs
@@ -2,8 +2,9 @@
 
 #[aoc::main($DAY)]
 fn main(input: &str) -> (usize, usize) {
+    let (mut p1, mut p2) = (0, 0);
     let xs = input.split('\n').map(|l| {
         l.split(',').collect::<Vec<_>>()
     }).collect::<Vec<_>>();
-    (0, 0)
+    (p1, p2)
 }
