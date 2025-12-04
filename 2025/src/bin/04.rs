996c8217b52a603c13b8a1532026aa3f5fe50472
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2025-12-04 06:28:27 +0100

    2025-04: clean up

--- 2025/src/bin/04.rs
+++ 2025/src/bin/04.rs
@@ -10,11 +10,11 @@ fn round(m: &mut [Vec<u8>], remove: bool) -> usize {
             if m[r][c] != b'@' {
                 continue;
             }
-            let n = dd.iter().filter(|&&(dr, dc)| {
-                let rr = (r as i64 + dr) as usize;
-                let cc = (c as i64 + dc) as usize;
-                m.get(rr).and_then(|row| row.get(cc)).is_some_and(|&x| x == b'@')
-            }).count();
+            let n = dd.iter().filter(|&&(dr, dc)|
+                m.get((r as i64 + dr) as usize)
+                    .and_then(|row| row.get((c as i64 + dc) as usize))
+                    .is_some_and(|&x| x == b'@')
+            ).count();
             if n < 4 {
                 if remove {
                     m[r][c] = b'.';
@@ -29,9 +29,10 @@ fn round(m: &mut [Vec<u8>], remove: bool) -> usize {
 #[aoc::main(04)]
 fn main(input: &str) -> (usize, usize) {
     let (mut p1, mut p2) = (0, 0);
-    let mut m = input.split('\n').map(|l| l.as_bytes().to_vec()).collect::<Vec<_>>();
+    let mut m = input.split('\n')
+        .map(|l| l.as_bytes().to_vec())
+        .collect::<Vec<_>>();
     p1 += round(&mut m, false);
-
     loop {
         let n = round(&mut m, true);
         if n == 0 {
