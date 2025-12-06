4c0ff7faa961a1ea74f8c65671b11711e0691f0e
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2025-12-06 06:44:33 +0100

    2025-06: clean up

--- 2025/src/bin/06.rs
+++ 2025/src/bin/06.rs
@@ -16,36 +16,32 @@ fn ans(problems: &[Vec<usize>], ops: &[u8]) -> usize {
 
 #[aoc::main(06)]
 fn main(input: &str) -> (usize, usize) {
-    let m = input.split('\n').collect::<Vec<_>>();
-    let ops = m.last().unwrap().bytes().filter(|&c| c != b' ').collect::<Vec<_>>();
+    let lines = input.split('\n').collect::<Vec<_>>();
+    let ops = lines[lines.len()-1].bytes().filter(|&c| c != b' ').collect::<Vec<_>>();
 
-    let mut problems1: Vec<Vec<usize>> = Vec::new();
-    for l in &m[0..m.len()-1] {
+    let mut p1: Vec<Vec<usize>> = Vec::new();
+    for l in &lines[0..lines.len()-1] {
         let ws = l.split_ascii_whitespace().collect::<Vec<_>>();
-        if problems1.is_empty() {
-            for _ in 0..ws.len() {
-                problems1.push(Vec::new());
-            }
-        }
         for i in 0..ws.len() {
-            problems1[i].push(ws[i].parse().unwrap())
+            if p1.len() <= i {
+                p1.push(Vec::new());
+            }
+            p1[i].push(ws[i].parse().unwrap())
         }
     }
 
-    let mut problems2: Vec<Vec<usize>> = Vec::new();
-    problems2.push(Vec::new());
-    for c in 0..m[0].len() {
-        let w = (0..m.len()-1)
-            .map(|r| m[r].as_bytes()[c] as char)
-            .filter(|&c| "0123456789".contains(c))
+    let mut p2: Vec<Vec<usize>> = Vec::from([Vec::new()]);
+    for c in 0..lines[0].len() {
+        let w = (0..lines.len()-1)
+            .map(|r| lines[r].as_bytes()[c] as char)
+            .filter(|&c| c.is_ascii_digit())
             .collect::<String>();
         if !w.is_empty() {
-            problems2.last_mut().unwrap().push(w.parse().unwrap());
+            p2.last_mut().unwrap().push(w.parse().unwrap());
         } else {
-            problems2.push(Vec::new());
+            p2.push(Vec::new());
         }
     }
-    let p1 = ans(&problems1, &ops);
-    let p2 = ans(&problems2, &ops);
-    (p1, p2)
+
+    (ans(&p1, &ops), ans(&p2, &ops))
 }
