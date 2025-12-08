f6b6ceba24f153379e6938cbc9a51eefc25c99c7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2025-12-08 06:37:16 +0100

    2025-08: complete

---
+++ 2025/src/bin/08.rs
@@ -0,0 +1,69 @@
+use std::collections::VecDeque;
+
+use hashbrown::{HashMap, HashSet};
+use itertools::Itertools;
+
+type Point = (usize, usize, usize);
+
+fn dist((x1, y1, z1): Point, (x2, y2, z2): Point) -> usize {
+    // we dont have to sqrt for sorting to still work
+    (x2 - x1).pow(2) + (y2 - y1).pow(2) + (z2 - z1).pow(2)
+}
+
+fn find_components(g: &HashMap<usize, HashSet<usize>>) -> Vec<usize> {
+    let mut visited = HashSet::new();
+    let mut components = Vec::new();
+    for &n in g.keys() {
+        if visited.contains(&n) {
+            continue;
+        }
+        let mut q = VecDeque::from([n]);
+        visited.insert(n);
+        let mut comp = 0;
+        while let Some(c) = q.pop_front() {
+            comp += 1;
+            for &n in &g[&c] {
+                if !visited.contains(&n) {
+                    visited.insert(n);
+                    q.push_back(n);
+                }
+            }
+        }
+        components.push(comp);
+    }
+    components
+}
+
+#[aoc::main(08)]
+fn main(input: &str) -> (usize, usize) {
+    let xs = input.split('\n').map(|l| {
+        let (x, rest) = l.split_once(',').unwrap();
+        let (y, z) = rest.split_once(',').unwrap();
+        (x.parse().unwrap(), y.parse().unwrap(), z.parse().unwrap())
+    }).collect::<Vec<Point>>();
+    let mut dist_pairs = (0..xs.len()).tuple_combinations().collect::<Vec<(usize, usize)>>();
+    dist_pairs.sort_by_key(|&(i, j)| dist(xs[i], xs[j]));
+    dist_pairs.reverse();
+    let mut g = HashMap::<usize, HashSet<usize>>::new();
+    let (mut p1, mut p2) = (0, 0);
+    for i in 0.. {
+        let (mut a, mut b) = dist_pairs.pop().unwrap();
+        while g.get(&a).is_some_and(|n| n.contains(&b)) {
+            (a, b) = dist_pairs.pop().unwrap();
+        }
+        g.entry(a).or_default().insert(b);
+        g.entry(b).or_default().insert(a);
+
+        let mut components = find_components(&g);
+        if i == 1000 {
+            components.sort();
+            p1 = components[components.len() - 3..].iter().product();
+            println!("{}", p1);
+        }
+        if components[0] == xs.len() {
+            p2 = xs[a].0 * xs[b].0;
+            break;
+        }
+    }
+    (p1, p2)
+}
