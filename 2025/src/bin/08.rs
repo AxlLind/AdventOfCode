a044f8c6d35aa4eea5300892f2fe6b423052550b
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2025-12-08 07:12:44 +0100

    2025-08: 5x speed up by using a distjoint set

--- 2025/src/bin/08.rs
+++ 2025/src/bin/08.rs
@@ -1,6 +1,3 @@
-use std::collections::VecDeque;
-
-use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
 
 type Point = (usize, usize, usize);
@@ -10,28 +7,44 @@ fn dist((x1, y1, z1): Point, (x2, y2, z2): Point) -> usize {
     (x2 - x1).pow(2) + (y2 - y1).pow(2) + (z2 - z1).pow(2)
 }
 
-fn find_components(g: &HashMap<usize, HashSet<usize>>) -> Vec<usize> {
-    let mut visited = HashSet::new();
-    let mut components = Vec::new();
-    for &n in g.keys() {
-        if visited.contains(&n) {
-            continue;
+struct UnionFindSet {
+    parent: Vec<usize>,
+    sizes: Vec<usize>,
+}
+
+impl UnionFindSet {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            sizes: vec![1; n],
         }
-        let mut q = VecDeque::from([n]);
-        visited.insert(n);
-        let mut comp = 0;
-        while let Some(c) = q.pop_front() {
-            comp += 1;
-            for &n in &g[&c] {
-                if !visited.contains(&n) {
-                    visited.insert(n);
-                    q.push_back(n);
-                }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find(self.parent[x]);
+        }
+        self.parent[x]
+    }
+
+    fn union(&mut self, a: usize, b: usize) {
+        let ra = self.find(a);
+        let rb = self.find(b);
+        if ra != rb {
+            if self.sizes[ra] < self.sizes[rb] {
+                self.parent[ra] = rb;
+                self.sizes[rb] += self.sizes[ra];
+            } else {
+                self.parent[rb] = ra;
+                self.sizes[ra] += self.sizes[rb];
             }
         }
-        components.push(comp);
     }
-    components
+
+    fn comp_size(&mut self, a: usize) -> usize {
+        let x = self.find(a);
+        self.sizes[x]
+    }
 }
 
 #[aoc::main(08)]
@@ -41,22 +54,22 @@ fn main(input: &str) -> (usize, usize) {
         let (y, z) = rest.split_once(',').unwrap();
         (x.parse().unwrap(), y.parse().unwrap(), z.parse().unwrap())
     }).collect::<Vec<Point>>();
+
     let mut dist_pairs = (0..xs.len()).tuple_combinations().collect::<Vec<(usize, usize)>>();
     dist_pairs.sort_by_key(|&(i, j)| dist(xs[i], xs[j]));
     dist_pairs.reverse();
-    let mut g = HashMap::<usize, HashSet<usize>>::new();
+
+    let mut uf = UnionFindSet::new(xs.len());
     let (mut p1, mut p2) = (0, 0);
     for i in 0.. {
         let (a, b) = dist_pairs.pop().unwrap();
-        g.entry(a).or_default().insert(b);
-        g.entry(b).or_default().insert(a);
-
-        let mut components = find_components(&g);
+        uf.union(a, b);
         if i == 1000 {
+            let mut components = uf.sizes.clone();
             components.sort();
             p1 = components[components.len() - 3..].iter().product();
         }
-        if components[0] == xs.len() {
+        if uf.comp_size(a) == xs.len() {
             p2 = xs[a].0 * xs[b].0;
             break;
         }
