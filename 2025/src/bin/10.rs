9a49eb1913a650671c3cad61d4a12131a3425f24
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2025-12-10 07:07:09 +0100

    2025-10: simplify

--- 2025/src/bin/10.rs
+++ 2025/src/bin/10.rs
@@ -5,15 +5,11 @@ fn parse_list(s: &str) -> Vec<usize> {
     s[1..s.len()-1].split(',').map(|w| w.parse().unwrap()).collect()
 }
 
-type Machine = (String, Vec<Vec<usize>>, Vec<usize>);
-
-fn solve_p1((lights, buttons, _): &Machine) -> usize {
+fn solve_p1(buttons: &[Vec<usize>], lights: &str) -> usize {
     let goal = lights.bytes().map(|c| c == b'#').collect::<Vec<_>>();
-    let start_state = vec![false; lights.len()];
-    let mut q = VecDeque::new();
-    q.push_back((start_state.clone(), 0usize));
-
     let mut seen = HashSet::new();
+    let mut q = VecDeque::new();
+    q.push_back((vec![false; lights.len()], 0usize));
     while let Some((state, n)) = q.pop_front() {
         if seen.contains(&state) {
             continue;
@@ -22,7 +18,6 @@ fn solve_p1((lights, buttons, _): &Machine) -> usize {
             return n;
         }
         seen.insert(state.clone());
-
         for button in buttons {
             let mut next = state.clone();
             for &i in button {
@@ -31,16 +26,14 @@ fn solve_p1((lights, buttons, _): &Machine) -> usize {
             q.push_back((next, n + 1));
         }
     }
-
-    unreachable!("Goal must be reachable per problem statement");
-
+    unreachable!()
 }
 
-fn solve_p2((_, buttons, jolts): &Machine) -> usize {
+fn solve_p2(buttons: &[Vec<usize>], jolts: &[usize]) -> usize {
     use good_lp::*;
     let mut vars = variables!();
-    for i in 0..buttons.len() {
-        vars.add(variable().min(0).integer().name(format!("x{}", i)));
+    for _ in 0..buttons.len() {
+        vars.add(variable().min(0).integer());
     }
     let press_vars = vars.iter_variables_with_def().map(|(v, _)| v).collect::<Vec<_>>();
 
@@ -68,10 +61,9 @@ fn main(input: &str) -> (usize, usize) {
         let (buttons_str, jolts_str) = rest.rsplit_once(' ').unwrap();
         let lights = lights_str[1..lights_str.len()-1].to_string();
         let buttons = buttons_str.split(' ').map(parse_list).collect::<Vec<_>>();
-        let jolts = parse_list(jolts_str);
-        (lights, buttons, jolts)
-    }).collect::<Vec<Machine>>();
-    let p1 = machines.iter().map(solve_p1).sum();
-    let p2 = machines.iter().map(solve_p2).sum();
+        (lights, buttons, parse_list(jolts_str))
+    }).collect::<Vec<_>>();
+    let p1 = machines.iter().map(|m| solve_p1(&m.1, &m.0)).sum();
+    let p2 = machines.iter().map(|m| solve_p2(&m.1, &m.2)).sum();
     (p1, p2)
 }
